b1761d06eeff3f813f9f4e74b040a3cc001ef7f8
unknown <leonardoxavier092@gmail.com>
2022-02-09 14:35:33 -0300

    first-commit

---
+++ js/owl/main.js
@@ -0,0 +1,36 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:8,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+/* const boxFilmes = document.querySelectorAll('.box-filmes')
+const carrossel = document.querySelector('.owl-carousel')
+
+carrossel.addEventListener('mousemove', (event)=>{
+    
+    if(event.target.parentNode.parentNode.style.zIndex == '1') return;
+
+    boxFilmes.forEach(element => {
+
+        if(element.parentNode.parentNode.style.zIndex = '1'){
+            element.parentNode.parentNode.style.zIndex = '0' 
+            return false; 
+        }        
+        element.parentNode.parentNode.style.zIndex = '0' 
+        console.log(element.parentNode.parentNode)
+    });
+
+    event.target.parentNode.parentNode.style.zIndex = '1'
+}) */
