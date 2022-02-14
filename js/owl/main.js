9d270b20d744425d9299f9077fa04ef5b8f321a3
unknown <leonardoxavier092@gmail.com>
2022-02-14 19:49:29 -0300

    edit: header+javascript-position-fixed

--- js/owl/main.js
+++ js/owl/main.js
@@ -36,3 +36,16 @@ carrossel.addEventListener('mousemove', (event)=>{
     event.target.parentNode.parentNode.style.zIndex = '1'
     event.target.parentNode.parentNode.style.overflow = 'visible';
 }) 
+
+window.onscroll = function() {myFunction()};
+        
+        var header = document.getElementById("myHeader");
+        var sticky = header.offsetTop;
+        
+        function myFunction() {
+          if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+          } else {
+            header.classList.remove("sticky");
+          }
+        }
